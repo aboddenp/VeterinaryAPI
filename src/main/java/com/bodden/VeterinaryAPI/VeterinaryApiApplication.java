9cdbff2acdf7c17bc510a4bb5cd373fdb6391694
boddenme <43423531+aboddenp@users.noreply.github.com>
2021-09-08 18:11:21 -0600

    Initial commit

---
+++ src/main/java/com/bodden/VeterinaryAPI/VeterinaryApiApplication.java
@@ -0,0 +1,13 @@
+package com.bodden.VeterinaryAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinaryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinaryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bodden/VeterinaryAPI/VeterinaryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bodden.VeterinaryAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinaryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
