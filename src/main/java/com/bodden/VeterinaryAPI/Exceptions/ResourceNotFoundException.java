d7acf8af0af66a4d03bbb9debacb3945e5b0e105
boddenme <43423531+aboddenp@users.noreply.github.com>
2021-09-10 16:31:46 -0600

    Update ResourceNotFoundException.java
    Make it return 404 code instead of server error

--- src/main/java/com/bodden/VeterinaryAPI/Exceptions/ResourceNotFoundException.java
+++ src/main/java/com/bodden/VeterinaryAPI/Exceptions/ResourceNotFoundException.java
@@ -1,5 +1,9 @@
 package com.bodden.VeterinaryAPI.Exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.NOT_FOUND, reason="Resource not found")
 public class ResourceNotFoundException extends RuntimeException{
     public ResourceNotFoundException() {
         super();
