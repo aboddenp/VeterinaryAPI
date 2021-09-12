7d331f175b6c80aed9f2ee42e30e1a34feddcfea
boddenme <43423531+aboddenp@users.noreply.github.com>
2021-09-12 16:54:08 -0600

    Update VeterinaryApiApplication.java
    Add bean

--- src/main/java/com/bodden/VeterinaryAPI/VeterinaryApiApplication.java
+++ src/main/java/com/bodden/VeterinaryAPI/VeterinaryApiApplication.java
@@ -1,13 +1,24 @@
 package com.bodden.VeterinaryAPI;
 
+import com.bodden.VeterinaryAPI.Services.AppointmentService;
+import com.bodden.VeterinaryAPI.Services.AppointmentServiceDefault;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		SecurityAutoConfiguration.class
+})
 public class VeterinaryApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VeterinaryApiApplication.class, args);
 	}
 
+	@Bean
+	public AppointmentService mainService(){
+		return new AppointmentServiceDefault();
+	}
 }
